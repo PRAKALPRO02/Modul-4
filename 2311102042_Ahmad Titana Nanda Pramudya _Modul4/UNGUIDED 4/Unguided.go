c85c7a885ea6057e6297cfd7c20034b2b29f5fb3
AhmadTitanaNandaPramudya <162532793+AhmadTitanaNandaPramudya@users.noreply.github.com>
2024-10-09 20:25:28 +0700

    Add files via upload

---
+++ 2311102042_Ahmad Titana Nanda Pramudya _Modul4/UNGUIDED 4/Unguided.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func cetakDeret(n int) {
+	
+	for n != 1 {
+		fmt.Printf("%d ", n)
+		if n%2 == 0 { 
+			n = n / 2
+		} else { 
+			n = 3*n + 1
+		}
+	}
+
+	fmt.Println(n)
+}
+
+func main() {
+	var n int
+	fmt.Print("Masukkan bilangan awal: ")
+	fmt.Scan(&n)
+
+	
+	if n > 0 && n < 1000000 {
+		cetakDeret(n)
+	} else {
+		fmt.Println("Input harus bilangan positif dan kurang dari 1000000.")
+	}
+}
