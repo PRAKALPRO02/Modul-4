93928adc9c0c04e274ee1787c28b5ecd06585c78
arnadd72 <149177348+arnadd72@users.noreply.github.com>
2024-10-09 14:47:27 +0700

    Add files via upload

---
+++ ARNANDA SETYA NOSA PUTRA_2311102180/unguided modul 4/soal no 3.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func cetakDeret(n int) {
+	for n != 1 {
+		fmt.Printf("%d ", n)
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+	}
+	fmt.Println(n)
+}
+
+func main() {
+	var n int
+	fmt.Println("Masukkan nilai suku awal (integer positif kurang dari 1000000): ")
+	fmt.Scan(&n)
+
+	if n > 0 && n < 1000000 {
+		cetakDeret(n)
+	} else {
+		fmt.Println("Masukan tidak valid. Masukkan angka positif kurang dari 1000000.")
+	}
+}
