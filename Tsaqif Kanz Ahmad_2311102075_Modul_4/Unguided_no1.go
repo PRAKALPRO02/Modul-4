a0444bcd2095433e5c6a91db1e58d91ee5d06ee4
TSAQIFKANZ <162876126+TSAQIFKANZ@users.noreply.github.com>
2024-10-12 20:46:51 +0700

    Add files via upload

---
+++ Tsaqif Kanz Ahmad_2311102075_Modul_4/Unguided_no1.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// Fungsi untuk mencetak deret Collatz
+func cetakDeret(n int) {
+	for n != 1 {
+		fmt.Printf("%d ", n)
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+	}
+	fmt.Println(n) // Cetak angka 1 di akhir
+}
+
+func main() {
+	var n int
+	fmt.Print("Masukkan bilangan awal: ")
+	fmt.Scan(&n)
+
+	// Validasi input
+	if n > 0 && n < 1000000 {
+		cetakDeret(n)
+	} else {
+		fmt.Println("Input harus bilangan positif dan kurang dari 1000000.")
+	}
+}
