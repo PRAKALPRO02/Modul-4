8f549f5648be353dff31197b653df6b64dfe30a6
yasvinsyahgana <163092106+yasvinsyahgana@users.noreply.github.com>
2024-10-15 00:26:46 +0700

    Update Unguided1.go

--- YASVIN SYAHGANA/UNGUIDED/Unguided1.go
+++ YASVIN SYAHGANA/UNGUIDED/Unguided1.go
@@ -1,49 +1,29 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-func main() {
-	var cx, cxx, cy, cyy, r, rr, x, y float64
+func printBaris(n int) {
 
-	fmt.Println("Lingkaran 1")
-	fmt.Print("(cx)(cy)(r) : ")
-	fmt.Scanln(&cx, &cy, &r)
-
-	fmt.Println("Lingkaran 2")
-	fmt.Print("(cx)(cy)(r) : ")
-	fmt.Scanln(&cxx, &cyy, &rr)
-
-	fmt.Print("Input koordinat sembarang (x)(y) : ")
-	fmt.Scanln(&x, &y)
-
-	//mari kita cek posisi Lingkaran1 dang Lingkaran2
-	posisi1 := didalam(cx, cy, x, y, r)
-	posisi2 := didalam(cxx, cyy, x, y, rr)
-
-	if posisi1 && posisi2 {
-		fmt.Println("Titik di dalam lingkaran 1 dan 2")
-	} else if posisi1 {
-		fmt.Println("Titik di dalam lingkaran 1")
-	} else if posisi2 {
-		fmt.Println("Titik di dalam lingkaran 2")
-	} else {
-		fmt.Println("Titik di luar lingkaran 1 dan 2")
+	for n != 1 {
+		fmt.Printf("%d ", n)
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
 	}
 
+	fmt.Println(n)
 }
 
-func jarak(a, b, c, d float64) float64 {
-	//pengecekan Jarak
-	kiri := (a - c) * (a - c)
-	kanan := (b - d) * (b - d)
-	return math.Sqrt(kiri + kanan)
-	//math.Sqrt digunakan untuk pemanggilan akar
-}
+func main() {
+	var n int
+	fmt.Print("Input (n): ")
+	fmt.Scan(&n)
 
-func didalam(cx, cy, x, y, r float64) bool {
-	//pengecekan apakah x, y berada dalam lingkaran
-	return jarak(cx, cy, x, y) <= r
+	if n > 0 && n < 1000000 {
+		printBaris(n)
+	} else {
+		fmt.Println("Tidak Memenuhi kondisi 'n > 0 && n < 1000000'")
+	}
 }
