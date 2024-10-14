7e5c79149449115c18b9dcbc454b8417b09f8758
damaragaluhhh <140993139+daamaarrrr@users.noreply.github.com>
2024-10-14 09:29:21 +0700

    Add files via upload

---
+++ Damara Galuh Pembayun/unguided.go
@@ -0,0 +1,21 @@
+package main
+//Damara Galuh Pembayun 23111202110
+import "fmt"
+
+func cetakDeret(n int) {
+        for n != 1 {
+                fmt.Print(n, " ")
+                if n%2 == 0 {
+                        n /= 2
+                } else {
+                        n = 3*n + 1
+                }
+        }
+        fmt.Println(1)
+}
+
+func main() {
+        var n int
+        fmt.Scan(&n)
+        cetakDeret(n)
+}
