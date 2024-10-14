d77ed5826bbbfeea7dab9d07d91825cef4a89105
yasvinsyahgana <163092106+yasvinsyahgana@users.noreply.github.com>
2024-10-15 00:26:02 +0700

    Update guided2.go

--- YASVIN SYAHGANA/GUIDED/guided2.go
+++ YASVIN SYAHGANA/GUIDED/guided2.go
@@ -1,49 +1,23 @@
-// package main
+package main
 
-// import "fmt"
+import "fmt"
 
-// // Fungsi buat menghitung faktorial
-// func factorial(n int) int {
-// 	if n == 0 || n == 1 {
-// 		return 1
-// 	}
+func main() {
+	var bilangan int
+	var pesan string
 
-// 	result := 1
-// 	for i := 2; i <= n; i++ {
-// 		result *= i
-// 	}
-// 	return result
-// }
+	fmt.Scan(&bilangan, &pesan)
+	cetakPesan(pesan, bilangan)
+}
 
-// // Fungsi buat menghitung permutasi
-// func permutation(n, r int) int {
-// 	return factorial(n) / factorial(n-r)
-// }
-
-// // Fungsi buat menghitung kombinasi
-// func combination(n, r int) int {
-// 	return factorial(n) / (factorial(r) * factorial(n-r))
-// }
-
-// func main() {
-// 	// Input 4 bilangan
-// 	var a, b, c, d int
-// 	fmt.Print("Masukkan bilangan a, b, c, d (dengan spasi): ")
-// 	fmt.Scanf("%d %d %d %d", &a, &b, &c, &d)
-// 	// Cek syarat a >= c dan b >= d
-// 	if a >= c && b >= d {
-// 		// Menghitung permutasi dan kombinasi a dan c
-// 		permutasiAC := permutation(a, c)
-// 		kombinasiAC := combination(a, c)
-// 		// Menghitung permutasi dan kombinasi b dan d
-// 		permutasiBD := permutation(b, d)
-// 		kombinasiBD := combination(b, d)
-// 		// Output hasil
-// 		fmt.Println("Permutasi(a, c) dan Kombinasi(a, c):",
-// 			permutasiAC, kombinasiAC)
-// 		fmt.Println("Permutasi(b, d) dan Kombinasi(b, d):",
-// 			permutasiBD, kombinasiBD)
-// 	} else {
-// 		fmt.Println("Syarat a >= c dan b >= d tidak terpenuhi.")
-// 	}
-// }
+func cetakPesan(M string, flag int) {
+	var jenis string
+	if flag == 0 {
+		jenis = "error"
+	} else if flag == 1 {
+		jenis = "warning"
+	} else if flag == 2 {
+		jenis = "informasi"
+	}
+	fmt.Println(M, jenis)
+}
